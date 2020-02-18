1455ba723d0baefc21d9b762e6056a8ced69d7c3
Larko_0 <larionov1001@gmail.com>
2020-02-18 10:50:43 +0200

    Deleted models Born, Die, Purchase... Only Change

--- history/admin.py
+++ history/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Move)
-admin.site.register(Purchase)
 admin.site.register(History)
+admin.site.register(Change)
--- history/models.py
+++ history/models.py
@@ -12,26 +12,8 @@ class Change(models.Model):
     total_weight = models.FloatField(default=0)
     description = models.CharField(max_length=300, default='')
     history = models.ForeignKey(History, null=True, on_delete=models.SET_NULL)
-
-
-class Move(Change):
     from_cell = models.ForeignKey(Cell, null=True, on_delete=models.SET_NULL, related_name='all_moves_from')
     to_cell = models.ForeignKey(Cell, null=True, on_delete=models.SET_NULL, related_name='all_moves_to')
+    partner = models.CharField(max_length=50, default='')
+    type = models.CharField(max_length=20, default='')
 
-
-class Purchase(Change):
-    supplier = models.CharField(max_length=50, default='')
-    to_cell = models.ForeignKey(Cell, null=True, on_delete=models.SET_NULL)
-
-
-class Sale(Change):
-    from_cell = models.ForeignKey(Cell, null=True, on_delete=models.SET_NULL)
-    client = models.CharField(max_length=50, default='')
-
-
-class Die(Change):
-    from_cell = models.ForeignKey(Cell, null=True, on_delete=models.SET_NULL)
-
-
-class Born(Change):
-    to_cell = models.ForeignKey(Cell, null=True, on_delete=models.SET_NULL)
